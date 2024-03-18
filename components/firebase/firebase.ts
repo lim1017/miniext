f72f5ce92879b43ec8f7954dfa54750bd8dd52f4
tom <lim1017@hotmail.com>
2024-03-18 12:43:10 -0400

    inital project setup

---
+++ components/firebase/firebase.ts
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+import { /* connectFirestoreEmulator, */ getFirestore } from 'firebase/firestore';
+// import { /* connectStorageEmulator, */ getStorage } from 'firebase/storage';
+// import { isDev } from '../isDev';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyB5jMP65p05bT0VrBYfXO65_HnnHBN-igM',
+    authDomain: 'miniext-lim1017.firebaseapp.com',
+    projectId: 'miniext-lim1017',
+    storageBucket: 'miniext-lim1017.appspot.com',
+    messagingSenderId: '720306543308',
+    appId: '1:720306543308:web:66192214421a353a0dc069',
+    measurementId: 'G-NHK9YQCJDC',
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+
+export const firebaseAuth = getAuth(firebaseApp);
+
+export const firestore = getFirestore(firebaseApp);
+export const baseBucketName = 'miniext-lim1017.appspot.com';
+
+/* if (isDev) {
+    connectFirestoreEmulator(firestore, '127.0.0.1', 8081);
+} */
